6e42fdf3f079d265f69d7e0bbacca2f4acd44984
tkgstrator <nasawake.am@gmail.com>
2025-08-09 08:57:22 +0000

    refactor(extensions): remove commented-out code and improve error logging

--- Sources/KaraokeSDK/Extensions/URLRequest.swift
+++ Sources/KaraokeSDK/Extensions/URLRequest.swift
@@ -83,8 +83,8 @@ extension URLRequest {
             }
 
             if type.contains("application/x-www-form-urlencoded") {
+                Logger.error("Merging credential with existing URL-encoded parameters.")
                 fatalError("URL-encoded parameters are not supported in this context.")
-//                Logger.debug("Merging credential with existing URL-encoded parameters.")
             }
         }
 
@@ -110,45 +110,9 @@ extension URLRequest {
             }
 
             if type.contains("application/x-www-form-urlencoded") {
+                Logger.error("Merging credential with existing URL-encoded parameters.")
                 fatalError("URL-encoded parameters are not supported in this context.")
-//                Logger.debug("Merging credential with existing URL-encoded parameters.")
             }
         }
-
-        // Parameters
-
-        // Assuming DkCredential has properties that can be added to the URLRequest
-//        if let body = httpBody,
-//           let parameters = try? JSONSerialization.jsonObject(with: body) as? [String: Any]
-//        {
-//            Logger.debug("Merging credential with existing parameters: \(parameters)")
-//            if targetUrl.path.hasPrefix("/minsei") {
-//                headers.add(name: "dmk-access-key", value: credential.dmkAccessKey)
-//                httpBody = try? JSONSerialization.data(withJSONObject: parameters.merging([
-//                    "authToken": "22d1e1712a41a25f2df83fc4d5facd0e",
-//                    "compAuthKey": credential.compAuthKey,
-//                    "userCode": credential.dtm.damtomoId,
-//                    "compId": credential.compId,
-//                ], uniquingKeysWith: { $1 }))
-//            }
-//            // 楽曲検索など
-//            if targetUrl.path.hasPrefix("/dkwebsys") {
-//                Logger.debug("URL path starts with 'dkwebsys', merging credential into parameters.")
-//                headers.add(name: "dmk-access-key", value: credential.dmkAccessKey)
-//                httpBody = try? JSONSerialization.data(withJSONObject: parameters.merging([
-//                    "authKey": credential.compAuthKey,
-//                    "compId": credential.compId,
-//                ], uniquingKeysWith: { $1 }))
-//            }
-//            // 筐体との連携など
-//            if targetUrl.path.hasPrefix("/dkdenmoku") {
-//                Logger.debug("URL path starts with 'dkdenmoku', merging credential into parameters.")
-//                httpBody = try? JSONSerialization.data(withJSONObject: parameters.merging([
-//                    "deviceId": credential.dtm.deviceId,
-//                    "cdmNo": credential.dtm.cdmNo,
-//                ], uniquingKeysWith: { $1 }))
-//            }
-//            return
-//        }
     }
 }
